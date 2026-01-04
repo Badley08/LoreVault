afa07d6b0cdffa30d5186752e07d3a4bef5f065b
Luberisse Karl Brad Sael Badley <karlluberisse1308@gmail.com>
2026-01-04 12:22:32 -0400

    Refactor service worker for cache management

--- sw.js
+++ sw.js
@@ -1,23 +1,10 @@
-const CACHE_NAME = 'lorevault-v2';
-const assets = [
-  './',
-  './index.html',
-  './lorevault.png',
-  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
-];
+const CACHE_NAME = 'lorevault-v1';
+const assets = ['./', './index.html', './style.css', './script.js', './lorevault.png'];
 
-self.addEventListener('install', evt => {
-  evt.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      cache.addAll(assets);
-    })
-  );
+self.addEventListener('install', e => {
+    e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
 });
 
-self.addEventListener('fetch', evt => {
-  evt.respondWith(
-    caches.match(evt.request).then(res => {
-      return res || fetch(evt.request);
-    })
-  );
+self.addEventListener('fetch', e => {
+    e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
